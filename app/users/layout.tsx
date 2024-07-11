e10d942fd7f2f63ca3db4dbcc5b26a75a951c771
Arihant Sharma <101751599+yuuaki1@users.noreply.github.com>
2024-07-11 14:01:38 +0530

    so many errors

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -5,7 +5,6 @@ import UserList from "./components/UserList";
 export default async function UsersLayout({children} : {children: React.ReactNode}) {
     const users = await getUsers();
     return (
-        //@ts-expect-error Server Component
         <Sidebar>
             <div className="h-full bg-neutral-900">
                 <UserList items = {users} />
